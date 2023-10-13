494cb6aca1aed2b3d40f04bacd285562aca543bc
Vazzzi69 <144496685+Vazzzi69@users.noreply.github.com>
2023-10-13 14:11:30 -0400

    Finished Q3
    Added the correlation matrix, not quite sure i can see a significant correlation yet, maybe i can better graph the data visualization to see it better?

--- AER-850 Project 1.py
+++ AER-850 Project 1.py
@@ -22,19 +22,31 @@ x = df['X']
 y = df['Y']
 z = df['Z']
 step = df['Step']
-
-
+ 
 
     # Scatter plot 
     
 fig1= plt.figure()
 
 ax1= fig1.add_subplot(111, projection='3d')
-ax1.scatter(x[step], y[step], z[step])
+ax1.scatter(x, y, z)
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')    
 ax1.set_zlabel('Z')
-ax1.set_title('Coordinates plot for maintinance steps') # check spelling 
- 
+ax1.set_title('Coordinates visualization Plot for Maintenance Steps')
+
 plt.show()
+
+# correlation analysis 
+
+import seaborn as sns 
+
+crmtrx =  df.drop(columns = "Step")
+corr_matrix = crmtrx.corr()
+sns.heatmap(corr_matrix)
+
+#Classification Model Development/Engineering
+
+
+
 
